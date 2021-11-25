037f3d08d96d1bf63b2ed1ab9134c5d3864f05e2
PedroEdu6786 <pedrcg835@gmail.com>
2021-11-25 15:27:06 -0600

    feature: add Deck model

---
+++ src/models/deckModel.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+const deckSchema = new mongoose.Schema(
+  {
+    cards: { type: Array, required: true },
+    name: { type: String, required: true },
+  },
+  { timestamps: true }
+)
+
+export const Deck = mongoose.model('Deck', deckSchema)
