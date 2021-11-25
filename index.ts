17dbb4c188da3ef909c45781b8d27d8b9c17b220
PedroEdu6786 <pedrcg835@gmail.com>
2021-11-25 13:46:13 -0600

    feature: add user register and login routes

--- index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Application, Request, Response } from 'express'
-
-const app: Application = express()
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Test')
-})
-
-app.listen('5000', () => console.log('Running'))
