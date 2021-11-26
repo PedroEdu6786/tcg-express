e532b27a41a12299a3b4da80053e48eccf5a607e
PedroEdu6786 <pedrcg835@gmail.com>
2021-11-25 18:55:02 -0600

    feature: add deck model

--- src/models/deckModel.ts
+++ src/models/deckModel.ts
@@ -4,6 +4,7 @@ const deckSchema = new mongoose.Schema(
   {
     cards: { type: Array, required: true },
     name: { type: String, required: true },
+    userId: { type: String, required: true },
   },
   { timestamps: true }
 )
