be18a4a6d87922ed4117c7a0058daedba77b0b28
PedroEdu6786 <pedrcg835@gmail.com>
2021-11-25 18:56:01 -0600

    feature: add auth middleware

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express"
+import jwt from 'jsonwebtoken'
+import { User } from "../models/userModel"
+
+
+const JWT_SECRET: string = process.env.JWT_SECRET!
+
+export const protect = async (req: any, res: Response, next: NextFunction) => {
+  let token
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1]
+      const decoded: any = jwt.verify(token, 'qwerty')
+
+      req.user = await User.findById(decoded.id)
+
+      next()
+    } catch (err) {
+      console.error(err)
+      res.status(401)
+      throw new Error('Not authorized, token failed')
+    }
+  }
+
+  if (!token) {
+    res.status(404)
+    throw new Error('Not authorized, no token')
+  }
+}
