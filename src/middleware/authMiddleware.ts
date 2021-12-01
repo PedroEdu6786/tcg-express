d8e7ef7c1e91faede27666f7ca0ce28d36980720
PedroEdu6786 <pedrcg835@gmail.com>
2021-11-30 18:48:11 -0600

    Refactor: add jwt secret as enviroment variable

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -13,7 +13,7 @@ export const protect = async (req: any, res: Response, next: NextFunction) => {
   ) {
     try {
       token = req.headers.authorization.split(' ')[1]
-      const decoded: any = jwt.verify(token, 'qwerty')
+      const decoded: any = jwt.verify(token, JWT_SECRET)
 
       req.user = await User.findById(decoded.id)
 
